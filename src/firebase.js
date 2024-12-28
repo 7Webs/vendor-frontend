7ddca0644e8b46891a20add6117c76753a8db994
tanay388 <tanay.deo388@gmail.com>
2024-12-28 22:16:45 +0530

    fix: page

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC6dimgR9F_4HCeju8UUXDy1iaPW_zphak",
-    authDomain: "sosal-31d0f.firebaseapp.com",
-    projectId: "sosal-31d0f",
-    storageBucket: "sosal-31d0f.appspot.com",
-    messagingSenderId: "455142259889",
-    appId: "1:455142259889:web:77a1a642515316b7d7b690",
-    measurementId: "G-2NHL2PSEV2"
+  apiKey: "AIzaSyAGB6Gf53bvceHpRmbKpk76x3H14mlp-OI",
+  authDomain: "nanoinfluencers-12ca2.firebaseapp.com",
+  projectId: "nanoinfluencers-12ca2",
+  storageBucket: "nanoinfluencers-12ca2.firebasestorage.app",
+  messagingSenderId: "695229200474",
+  appId: "1:695229200474:web:2a8ea1cc4fa65f85c28063",
+  measurementId: "G-CM9ZP7BG1K",
 };
 
 // Initialize Firebase
